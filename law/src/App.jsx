3dfd0f40f42d151c164efd7ed0e8e148c22f57bf
Rohit <rohit5003patil@gmail.com>
2025-07-23 18:42:21 +0530

    feat: add main application structure with routing and components

---
+++ law/src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HeroSection from './HeroSection';
+import AuthPage from './AuthPage';
+import IndiaBixHome from './Home';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HeroSection />} /> {/* Landing page */}
+        <Route path="/home" element={<IndiaBixHome />} /> {/* After login/signup */}
+        <Route path="/login" element={<AuthPage isLogin={true} />} />
+        <Route path="/signup" element={<AuthPage isLogin={false} />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
