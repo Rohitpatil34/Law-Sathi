70edd20edb04a2a7ce37fae9a6fb795a1b952c36
Rohit patil <136801508+Rohitpatil34@users.noreply.github.com>
2025-08-10 22:25:08 +0530

    feat: add actRoutes for handling routes related to acts and categories

---
+++ backend/Routes/actRoutes.js
@@ -0,0 +1,22 @@
+// Routes/actsRoutes.js
+import express from "express";
+import {
+  getAllMainCategories,
+  getSubcategoriesByMainCategory,
+  getActsByCategory,
+} from "../Controllers/actController.js";
+
+const router = express.Router();
+
+// 1. Get all main categories
+router.get("/main-categories", getAllMainCategories);
+
+// 2. Get subcategories by main category
+router.get("/main-category/:mainCategory", getSubcategoriesByMainCategory);
+
+// 3. Get acts by category
+router.get("/category/:category", getActsByCategory);
+
+
+
+export default router;
